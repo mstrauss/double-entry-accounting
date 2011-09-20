f4f9e133a51445be496ec08cacbcd62097bc5da2
Markus Strauss <Markus@ITstrauss.eu>
2011-09-20 18:43:41 -0400

    Initial commit.
    Empty rails app created with 'rails new double_entry_accounting -d mysql -T'
    Rails version: 3.1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoubleEntryAccounting::Application.config.secret_token = '01e988231c042c180ce46e16fc160727bf8326e3966d3bbb5e89fd910bbd20565bbb553f72616dce00f7584c63aa7adf920e42cdf93878c079bc0316061f6313'
