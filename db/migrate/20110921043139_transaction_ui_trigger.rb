a2c0fef9852907c666e0b7a129224ce15c24f287
Markus Strauss <Markus@ITstrauss.eu>
2011-09-21 17:01:06 -0400

    trigger to prohibit modifications of locked transactions on db level

---
+++ db/migrate/20110921043139_transaction_ui_trigger.rb
@@ -0,0 +1,25 @@
+class TransactionUiTrigger < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      create or replace function check_update_allowed()
+        returns trigger as $F$
+      begin
+        if OLD.locked then
+          raise exception $$This record is locked and does not allow updating.$$;
+        else
+          return NEW;
+        end if;
+      end
+      $F$ language plpgsql;
+
+      create trigger check_update_allowed_u_trigger
+        before update
+        on transactions
+        for each row
+        execute procedure check_update_allowed();
+    SQL
+  end
+
+  def down
+  end
+end
