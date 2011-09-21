e4a4904667b384eecd074b4d6b2d4a04e0fa36de
Markus Strauss <Markus@ITstrauss.eu>
2011-09-21 18:17:54 -0400

    more specs

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -26,6 +26,21 @@ def db_should_not_allow_update_attribute( name, value )
   end
 end
 
+shared_examples_for 'any locked or reconciled transaction' do
+  it 'should not be possible to change the amount' do
+    db_should_not_allow_update_attribute( :amount, 99 ) end
+  it 'should not be possible to change the date' do
+    db_should_not_allow_update_attribute( :date, Date.today - 5.days ) end
+  it 'should not be possible to change the transaction text' do
+    db_should_not_allow_update_attribute( :text, 'new transaction text' ) end
+  it 'should not be possible to change the debit account' do
+    db_should_not_allow_update_attribute( :debit_account_id, 123 ) end
+  it 'should not be possible to change the credit account' do
+    db_should_not_allow_update_attribute( :credit_account_id, 123 ) end
+  it 'should not be possible to change the transaction notes' do
+    db_should_not_allow_update_attribute( :notes, 'some new notes' ) end      
+end
+
 describe Transaction do
 
   before(:each) do
@@ -72,42 +87,62 @@ describe Transaction do
       @t.reload
     end
 
-    it 'should not be possible to change the amount' do
-      db_should_not_allow_update_attribute( :amount, 99 ) end
-    it 'should not be possible to change the date' do
-      db_should_not_allow_update_attribute( :date, Date.today - 5.days ) end
-    it 'should not be possible to change the transaction text' do
-      db_should_not_allow_update_attribute( :text, 'new transaction text' ) end
-    it 'should not be possible to change the debit account' do
-      db_should_not_allow_update_attribute( :debit_account_id, 123 ) end
-    it 'should not be possible to change the credit account' do
-      db_should_not_allow_update_attribute( :credit_account_id, 123 ) end
-    it 'should not be possible to change the transaction notes' do
-      db_should_not_allow_update_attribute( :notes, 'some new notes' ) end      
+    it_behaves_like 'any locked or reconciled transaction'
+
     it 'should not be possible to reconcile the transaction' do
       db_should_not_allow_update_attribute( :reconciled, true ) end      
 
-    it 'should set the locked_at datetime when locked'
-    it 'should reset the locked_at datetime when unlocked'
+    it 'should set the locked_at datetime when locked' do
+      @t.locked_at.should == DateTime.now
+    end      
+    it 'should reset the locked_at datetime when unlocked' do
+      @t.update_attribute( :locked, false )
+      @t.locked_at.should == nil
+    end
     
-    it 'should be unlockable'
+    it 'should be unlockable' do
+      @t.update_attribute( :locked, false ).should == true
+    end
   end
 
   # 'reconciled' may be set once, but never reset
   describe 'when reconciled' do
-    it 'should not be possible to change the amount'
-    it 'should not be possible to change the date'
-    it 'should not be possible to change the transaction text'
-    it 'should not be possible to change the debit account'
-    it 'should not be possible to change the credit account'
-    it 'should be possible to change the transaction notes'
     
-    it 'should set the reconciled_at datetime when reconciled'
-    it 'should not be possibe to reset the reconciled flag'
+    before(:each) do
+      @t.reconciled = true
+      @t.save!
+      @t.reload
+    end
     
-    it 'should become locked when it gets reconciled'
+    it_behaves_like 'any locked or reconciled transaction'
+    
+    it 'should set the reconciled_at datetime when reconciled' do
+      @t.reconciled_at.should == DateTime.now
+    end
+    
+    it 'should not be possibe to reset the reconciled flag' do
+      test_for_db_error( /It is not possible to unreconcile a transaction after it has been reconciled/ ) do
+        @t.update_attribute( :reconciled, false )
+      end
+    end
+    
+    it 'should become locked when it gets reconciled' do
+      @t.locked.should == true
+    end
   end
 
-  it 'should save 3 significant decimal places of the amount'
+  def on_update_and_reload_amount_should_remain_unchanged( amount )
+    @t.update_attribute( :amount, amount )
+    @t.save!
+    @t.reload
+    @t.amount.should == BigDecimal.new(amount)
+  end
+
+  it 'should save all significant decimal places of the amount' do
+    on_update_and_reload_amount_should_remain_unchanged( '12.345' )
+    on_update_and_reload_amount_should_remain_unchanged( '123456789.123' )
+    on_update_and_reload_amount_should_remain_unchanged( '12345.6789' )
+    on_update_and_reload_amount_should_remain_unchanged( '1234567890123.998877665544' )
+  end
   
 end
