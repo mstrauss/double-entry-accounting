1d85c054ab2e607a3c67d694970caa5893455a79
Markus Strauss <Markus@ITstrauss.eu>
2011-11-06 19:18:20 -0500

    AccountType#saldo

--- app/models/account_type.rb
+++ app/models/account_type.rb
@@ -1,2 +1,8 @@
 class AccountType < ActiveRecord::Base
+  has_many :accounts
+
+  def saldo
+    self.accounts.inject(0) { |s,a| s += a.saldo }
+  end
+  
 end
--- spec/models/account_type_spec.rb
+++ spec/models/account_type_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
+def valid_account_type
+  AccountType.first
+end
+
 describe AccountType do
+
+  it 'should calculate the saldo for its sub-accounts' do
+    valid_account_type.respond_to?(:saldo).should == true
+  end
+  
+  describe 'saldo'
+  it 'should equal the saldo of all sub-accounts' do
+    valid_account_type.saldo.should == -52.5
+  end
 end
